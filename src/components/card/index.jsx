b20c66a3f61574cee561f620e259ec6572f5010a
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-08-27 00:24:37 +0600

    add card design and  show all card element

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,6 +1,33 @@
+/* eslint-disable react/prop-types */
+import { useSelector } from "react-redux";
+import moment from "moment";
 
-export default function Card({data}) {
+export default function Card({ data, tranding, index }) {
+  const { imageURL } = useSelector(state => state?.moviesData);
   return (
-    <div className="p-5 shadow-md">Card</div>
+    <div className="w-full h-80 shadow-md relative">
+      <img
+        src={imageURL + data?.poster_path}
+        alt="card-image"
+        className="w-full h-full"
+      />
+      <div className="absolute top-4">
+        {
+          tranding && (
+            <div className="px-4 py-1 bg-black/60 overflow-hidden backdrop-blur-3xl rounded-r-full ">
+              # {index + 1} Tranding
+            </div>
+          )
+        }
+      </div>
+      <div className="absolute bottom-0 bg-black/60 w-full h-16 backdrop-blur-3xl p-2">
+        <h2 className="text-ellipsis line-clamp-1 text-lg font-semibold"> {data?.title || data?.name}</h2>
+        <div className="text-neutral-400 text-base font-normal flex justify-between">
+          <p> {moment(data?.release_date).format("MMMM Do YYYY")}</p>
+          <p className="bg-gradient-to-l from-amber-600 to-red-700 pl-4 pr-2 text-sm text-white rating">Rating: {Number(data?.vote_average).toFixed(1)}</p>
+        </div>
+
+      </div>
+    </div>
   )
 }
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -7,16 +7,20 @@ export default function Home() {
   return (
     <div>
       <BannerHome />
-      <div>
-        <h1>Tranding Show</h1>
+      <div className="container mx-auto px-3 my-5">
+        <h1 className="text-xl lg:text-2xl font-bold">Tranding Show</h1>
+        <div  className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 mt-3">
         {
-        trandingMoviesData?.length > 0 && trandingMoviesData?.map(data => {
+        trandingMoviesData?.length > 0 && trandingMoviesData?.map((data, index) => {
           return <Card
             key={data?.id}
             data={data}
+            tranding={true}
+            index={index}
           />
         })
       }
+        </div>
       </div>
     </div>
   )
