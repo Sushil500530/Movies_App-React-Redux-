44c2accabe589fe2b0828474d750f9a22976120b
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-08-26 11:39:28 +0600

    add banner description in banner bottom

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -7,12 +7,12 @@ export default function BannerHome() {
     return (
         <section className="w-full h-full">
 
-            <div className="flex min-h-full max-h-screen">
+            <div className="flex min-h-full max-h-screen overflow-hidden">
                 {
                     bannerData?.length > 0 && bannerData.map((item) => {
-                        return <div 
-                        className="min-w-full min-h-[450px] lg:min-h-full overflow-hidden relative"
-                        key={item?.id}
+                        return <div
+                            className="min-w-full min-h-[450px] lg:min-h-full overflow-hidden relative"
+                            key={item?.id}
                         >
                             <div className="w-full h-full">
                                 <img
@@ -23,10 +23,20 @@ export default function BannerHome() {
                             </div>
                             <div className="absolute top-0 w-full h-full bg-gradient-to-t from-neutral-900 to-transparent"></div>
 
-
-                            <div className="container mx-aut absolute bottom-10 max-w-md px-3  ">
-                                <h1 className="font-bold text-2xl lg:text-4xl">{item?.title}</h1>
-                                <p className="text-ellipsis line-clamp-3">{item?.overview}</p>
+                            <div className="container mx-auto ">
+                                <div className="absolute bottom-10 max-w-md px-3  ">
+                                    <h1 className="font-bold text-2xl lg:text-4xl">{item?.title}</h1>
+                                    <p className="text-ellipsis line-clamp-3 my-2">{item?.overview}</p>
+                                    <div className=" flex items-center gap-5">
+                                        <h1>Rating : {Number(item?.vote_average).toFixed(1)}+</h1>
+                                        <span>|</span>
+                                        <h1>Views :
+                                            {Number(item?.popularity).toFixed(0)}</h1>
+                                    </div>
+                                    <button
+                                        className="bg-gradient-to-l from-amber-600 to-red-700 active:scale-90 transition-all font-normal text-base px-4 py-2 rounded mt-3"
+                                    >Play Now</button>
+                                </div>
                             </div>
                         </div>
                     })
