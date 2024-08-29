bf471e4b4183623011fb4338813da63a2c9f5965
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-08-29 10:21:37 +0600

    add now playing fetch data

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,9 +1,29 @@
 import { useSelector } from "react-redux";
 import BannerHome from "../../components/banner";
 import HorizontalScrollCard from "../../components/horizontal-scroll-card";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
   const trandingMoviesData = useSelector(state => state?.moviesData?.bannerData);
+  const [nowPlayingData,setNowplayingData]= useState([]);
+  
+  const nowFetchData = async()=> {
+    try {
+      const response = await axios.get('/movie/now_playing');
+      setNowplayingData(response?.data?.results)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+ console.log(nowPlayingData);
+  useEffect(()=> {
+    nowFetchData();
+  },[])
+
+
+
+
   return (
     <div>
       <BannerHome />
