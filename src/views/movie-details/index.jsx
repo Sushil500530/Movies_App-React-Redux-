574234aa89e8fd598ade930a751a6a343d652936
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-09-04 12:05:51 +0600

    add some details in movie,tv or another details page

--- src/views/movie-details/index.jsx
+++ src/views/movie-details/index.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom"
 import { useFetchDetailsData } from "../../hooks/fetch-details-data";
 import { useSelector } from "react-redux";
+import moment from "moment";
 
 export default function MoviesDetails() {
   const { id, explore } = useParams();
@@ -9,14 +10,14 @@ export default function MoviesDetails() {
   const { data: castData } = useFetchDetailsData(`/${explore}/${id}/credits`)
 
   console.log("details data: ", imageURL)
-
+  const duration = Number(detailsData?.runtime/60).toFixed(1)?.split(".")
   return (
     <div>
       {/* <h2 className="text-lg lg:text-xl my-3 font-semibold capitalize">
          Movies Details
         </h2> */}
         {/* banner section  */}
-      <div className="w-full h-[320px] relative">
+      <div className="w-full h-[320px] relative hidden lg:block">
         <div className="w-full h-full">
           <img
             src={imageURL+detailsData?.backdrop_path}
@@ -26,13 +27,37 @@ export default function MoviesDetails() {
         </div>
         <div className="absolute inset-0 h-full w-full bg-gradient-to-t from-neutral-900 to-transparent"></div>
       </div>
-       <div className="container mx-auto px-3 py-16 lg:py-0">
-        <div className="relative w-fit mx-auto lg:ml-0 lg:-mt-28">
+       <div className="container mx-auto px-3 py-16 lg:py-0 flex flex-col gap-5 lg:flex-row lg:gap-10 mt-1">
+        <div className="relative w-fit mx-auto lg:mx-0 lg:-mt-28">
           <img
               src={imageURL + detailsData?.poster_path}
               alt="details image"
-              className="w-80 h-80 object-cover rounded"
+              className="lg:w-60 w-80 h-80 object-cover rounded"
             />
+        </div>
+        <div>
+          <h1 className="text-2xl font-bold text-white">{detailsData?.title || detailsData?.name}</h1>
+          <p className="text-neutral-400">{detailsData?.tagline}</p>
+          <div>
+            <div className="flex gap-3 my-3">
+              <p>Rating : {Number(detailsData?.vote_average).toFixed(1)}+</p>
+              <span>|</span>  
+              <p>View : {Number(detailsData?.vote_count).toFixed(1)}</p>
+              <span>|</span>
+              <p>Duration : {duration[0]}h {duration[1]}m</p>
+            </div>
+          </div>
+          <div className="">
+            <h1 className="text-lg font-medium  text-white">Overview</h1>
+            <p className="text-neutral-400 my-1">{detailsData?.overview}</p>
+            <div className="flex items-center text-center gap-3 my-3">
+              <h1>Status: {detailsData?.status}</h1>
+              <span>|</span>
+               <p>Release Data :  {moment(detailsData?.release_date).format("MMM Do YYYY")}</p>
+               <span>|</span>
+               <p>Revenue :  {Number(detailsData?.revenue)}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
